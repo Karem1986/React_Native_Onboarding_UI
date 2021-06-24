66b144de2dbf149da4246246112af0a5f980353e
Karem1986 <karem.ortiz@outlook.com>
2021-06-24 10:06:04 +0200

    added onboarding screens

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, Image, StyleSheet} from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 
 export default function OnboardingScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Onboarding Screen</Text>
-      <Button title="Click here" onPress={() => navigation.navigate('Login')} />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Onboarding Screen</Text>
+    //   <Button title="Click here" onPress={() => navigation.navigate('Login')} />
+    // </View>
+
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: '#fff',
+          image: (
+            <Image source={require('../assets/peopleIllustration1.png')} />
+          ),
+          title: 'Onboarding 1: Welcome to ActiviFun',
+          subtitle: 'Done with React Native Onboarding swipper for you',
+        },
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('../assets/people2.png')} />,
+          title: 'Onboarding 2: Choose from activities or create your own',
+          subtitle: 'Done with React Native Onboarding swipper for you',
+        },
+      ]}
+    />
   );
 }
 
